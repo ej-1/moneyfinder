dcf18ad88d759719bce3568a25e85d4c8fa9f9d0
erikwjonsson <erikwjonsson@gmail.com>
2014-10-25 20:52:26 +0200

    Small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,3 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
-
-# Prevent initializing the application before assets are precompiled (required for heroku) 
-# config.assets.initialize_on_precompile = false 
-# Add Rails Admin assets (required) 
-# config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
