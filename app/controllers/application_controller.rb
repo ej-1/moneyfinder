976ccb19e6261e30563858ca0d837ebc3d7475e2
erikwjonsson <erikwjonsson@gmail.com>
2014-10-25 21:04:03 +0200

    Tried to fix admin page on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+# https://github.com/sferik/rails_admin/wiki/Troubleshoot to get rails_admin to work
+config.assets.enabled = true
+config.assets.initialize_on_precompile = false
+config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
