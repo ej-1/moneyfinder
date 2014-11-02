c854c91cf2852b36fc247937465a43ab01adaf0b
erikwjonsson <erikwjonsson@gmail.com>
2014-11-02 22:24:29 +0100

    test2 - to see if rails_admin will work on herok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  config.assets.enabled = true
+  config.assets.initialize_on_precompile = false
   protect_from_forgery with: :exception
 end
