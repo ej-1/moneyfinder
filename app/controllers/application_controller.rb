7d2ab0da56555b29b398b8d55d7c73d7b36b7a3b
erikwjonsson <erikwjonsson@gmail.com>
2014-11-02 22:28:19 +0100

    Changed back after test for rails_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  config.assets.initialize_on_precompile = false
   protect_from_forgery with: :exception
 end
