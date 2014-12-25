c4c014ce112aaa4c4c840f1fc61faf28a387b86c
erikwjonsson <erikwjonsson@gmail.com>
2014-12-25 19:03:25 +0100

    Got radiobutton to be preselected with its function on pageload

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -18,3 +18,4 @@ end
 # Be sure to use plural when naming controller. LoansController, not LoanController
 # See here how to create Controllers.
 # http://www.sitepoint.com/building-your-first-rails-application-views-and-controllers/
+
