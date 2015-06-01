ce6246470a22fa29e00274088a8bde5f81f26e0f
erikwjonsson <erikwjonsson@gmail.com>
2015-06-01 15:35:10 +0200

    Tried some things to get compression to work

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Ratesfinder
   class Application < Rails::Application
+
+    config.middleware.use Rack::Deflater # This uses Gzip to compress files for faster page load.
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
