2316adc72914bdefc8744db357f5192cfc049b80
erikwjonsson <erikwjonsson@gmail.com>
2015-06-02 15:39:49 +0200

    Tried to use yui compressor

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Ratesfinder
   class Application < Rails::Application
-
-    config.middleware.use Rack::Deflater # This uses Gzip to compress files for faster page load.
+    config.assets.js_compressor = :yui
+   
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
