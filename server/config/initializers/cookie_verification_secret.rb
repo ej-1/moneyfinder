a57b1d97fe7a61069f0d5b464206d3f87388bb30
erikwjonsson <erikwjonsson@gmail.com>
2015-06-11 16:16:12 +0200

    Deleting old .png files for logo images.

--- server/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '2b8f2a1cab84f23ef04ccdfa0225e8c4a146a141e5d7e36fe943bbc3964417b15d58da0ab8d6a24b8f07f0f0534d5d88eb752fd4e59f971541eaadaf0c41450d';
