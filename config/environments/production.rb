7c2ac20f17e91f0588ea5eaebe19b294c2076bb2
erikwjonsson <erikwjonsson@gmail.com>
2015-06-15 12:46:15 +0200

    set precompile assets to true.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  rake assets:precompile
+  rake assets:precompile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
