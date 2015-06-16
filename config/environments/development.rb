c0deeac13ac59171bff0f8a1eb13b45d7cb5c127
erikwjonsson <erikwjonsson@gmail.com>
2015-06-16 16:09:03 +0200

    Got caching to work locally. Time to get it to work online.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.assets.precompile << 'application.js'
   config.assets.precompile << 'application.css'
-  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
+  config.action_controller.perform_caching = true
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.assets.precompile << 'application.js'
   config.assets.precompile << 'application.css'
-  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
+  config.action_controller.perform_caching = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
