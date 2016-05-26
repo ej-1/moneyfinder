51d4fcd8e13b7c2e085bac0207e22b321d38d763
memesta <erikwjonsson@gmail.com>
2016-05-26 15:26:18 +0200

    Removed precompilation of application.js and application.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.precompile << 'application.js'
-  config.assets.precompile << 'application.css'
+  #config.assets.precompile << 'application.js'
+  #config.assets.precompile << 'application.css'
   config.action_controller.perform_caching = true
   # Code is not reloaded between requests.
   config.cache_classes = true
